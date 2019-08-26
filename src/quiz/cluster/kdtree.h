4ead76c401b257bd6d8ef04808cd76b5d1e2471d
Daniel Hunter <officialdanielhunter@gmail.com>
2019-08-26 11:20:13 -0700

    KD Tree Insert done, partial progress on search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -2,8 +2,7 @@
 
 
 // Structure to represent node of kd tree
-struct Node
-{
+struct Node {
 	std::vector<float> point;
 	int id;
 	Node* left;
@@ -14,8 +13,9 @@ struct Node
 	{}
 };
 
-struct KdTree
-{
+void insert_recur(Node **n, std::vector<float> point, int id, int depth);
+
+struct KdTree {
 	Node* root;
 
 	KdTree() : root(NULL) {}
@@ -23,21 +23,50 @@ struct KdTree
 	void insert(std::vector<float> point, int id) {
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root
-        Node new_node = Node(point, id);
-        if (root == NULL) {
-            root = &new_node; // root = address of new_node
-        }
+        insert_recur(&root, point, id, 0);
 	}
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+        search_recur(&root, &ids, &target, distanceTol, 0);
 		return ids;
 	}
-
 };
 
+void search_recur(Node **node, std::vector<int> &ids, const std::vector<float> target, const float distanceTol, int depth) {
+    // check if current node is a candidate
+    if (abs((*n)->point[0] - target[0])<distanceTol && abs((*n)->point[1] - target[1])<distanceTol) {
+        // TODO: do some circle math
+        ids->push_back((*n)->id);
+    }
+
+    // check if each child is a candidate
+    int cd = depth % 2;
+    if ((*n)->left.point[cd]
 
 
+void insert_recur(Node **n, std::vector<float> point, int id, int depth) {
+    if (*n == NULL) {
+        *n = new Node(point, id);
+        return;
+    }
+
+    float point_data = 0;
+    float node_data = 0;
+    if (depth % 2 == 0) { // even
+        point_data = point[0];
+        node_data = (*n)->point[0];
+    } else { // odd
+        point_data = point[1];
+        node_data = (*n)->point[1];
+    }
+
+    if (point_data < node_data) {
+        insert_recur(&((*n)->left), point, id, depth+1);
+    } else {
+        insert_recur(&((*n)->right), point, id, depth+1);
+    }
+}
 
